c553f6161181fa63678f5f7d0cd200540c06c11c
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-09 22:53:08 +0700

    Add files via upload

---
+++ src/snk.js
@@ -0,0 +1,10 @@
+const snk = () => { 
+	return `Syarat dan Ketentuan Bot *Rizky* 
+1. Kami tidak menyimpan gambar, video, file, audio, dan dokumen yang anda kirim
+2. Kami tidak akan pernah meminta anda untuk memberikan informasi pribadi
+3. Jika menemukan Bug/Error silahkan langsung lapor ke Owner bot
+4. Apapun yang anda perintah pada bot ini, KAMI TIDAK AKAN BERTANGGUNG JAWAB!
+
+Thanks !`
+}
+exports.snk = snk
