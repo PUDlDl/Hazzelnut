f3830cb6ec5159e795f8461bf943fadc03ec5a86
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 06:33:04 +0700

    Update daftarvip.js

--- src/daftarvip.js
+++ src/daftarvip.js
@@ -2,16 +2,16 @@ const daftarvip = (prefix) => {
 	return `
 	
 *HARGA DAFTAR VIP :*
--Rp. 5K > Akses Fitur ViP
--Rp. 10K > Fitur VIP + Masukin Bot KeGrup Kalian!
+-Rp. ?K > Akses Fitur ViP
+-Rp. ??K > Fitur VIP + Masukin Bot KeGrup Kalian!
 
 *JIKA INGIN DAFTAR VIP :*
+
 *Chat Owner BOT :*
-_wa.me/6282387804410 atau ketik *${prefix}owner*
+_wa.me/6287714745440 atau ketik *${prefix}owner*
 
 *NOTE*
 
-*GRUP WHATSAPP BOT :*
-_https://chat.whatsapp.com/DiqQCtQ1FPSJsC7lr8bqwO_ `
+Please wait for the manuscript in the manufacturing process `
 }
 exports.daftarvip = daftarvip
