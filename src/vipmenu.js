33e496232509801532807def3ad183aecc9ef9ae
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:51:50 +0700

    Update vipmenu.js

--- src/vipmenu.js
+++ src/vipmenu.js
@@ -1,28 +1,27 @@
 const vipmenu = (prefix) => { 
 	return `            
 	
-╔══✪〘 USER VIP 〙✪══
-║
-╠➥ *${prefix}ytmp4 [link]*
-╠➥ *${prefix}ytmp3 [link]*
-╠➥ *${prefix}hidetag2*
-╠➥ *${prefix}joox [lagu]*
-╠➥ *${prefix}setprefix*
-╠➥ *${prefix}tomp3 [replay video]*
-╠➥ *${prefix}randomanime*
-╠➥ *${prefix}randomhentai*
-╠➥ *${prefix}nsfwloli*
-╠➥ *${prefix}nsfwblowjob*
-╠➥ *${prefix}nsfwneko*
-╠➥ *${prefix}nsfwtrap*
-╠➥ *${prefix}indohot*
-╠➥ *${prefix}otagall2*
-╠➥ *${prefix}otagall3*
-╠➥ *${prefix}hidetag5*
-╠➥ *${prefix}indo(1-25)*
-╠➥ *CONTOH ${prefix}indo1*
-║
-╚══✪〘 Hazzelnut BOT 〙✪══
+╭◪ *VIP USER*
+╰───────────────────
+•├❏ *${prefix}ytmp4 [link]*
+•├❏ *${prefix}ytmp3 [link]*
+•├❏ *${prefix}hidetag2*
+•├❏ *${prefix}joox [lagu]*
+•├❏ *${prefix}setprefix*
+•├❏ *${prefix}tomp3 [replay video]*
+•├❏ *${prefix}randomanime*
+•├❏ *${prefix}randomhentai*
+•├❏ *${prefix}nsfwloli*
+•├❏ *${prefix}nsfwblowjob*
+•├❏ *${prefix}nsfwneko*
+•├❏ *${prefix}nsfwtrap*
+•├❏ *${prefix}indohot*
+•├❏ *${prefix}otagall2*
+•├❏ *${prefix}otagall3*
+•├❏ *${prefix}hidetag5*
+•├❏ *${prefix}indo(1-25)*
+•├❏ *CONTOH ${prefix}indo1*
+ ╰──────────────❏ Hazzelnut BOT ❏
 `
 }
 exports.vipmenu = vipmenu
