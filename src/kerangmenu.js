c49a1a8b1f979f59be22f1ba631e24fade52542d
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:20:24 +0700

    Update kerangmenu.js

--- src/kerangmenu.js
+++ src/kerangmenu.js
@@ -1,16 +1,16 @@
 const kerangmenu = (prefix) => { 
 	return `
-╔══✪〘 KERANG 〙✪══
-║
-╠➥ *${prefix}apakah [optional]*
-╠➥ *${prefix}rate [optional]*
-╠➥ *${prefix}bisakah [optional]*
-╠➥ *${prefix}kapankah [optional]*
-╠➥ *${prefix}gantengcek*
-╠➥ *${prefix}toxic*
-╠➥ *${prefix}cantikcek*
-╠➥ *${prefix}persengay*
-║
-╚═〘 Hazzelnut BOT 〙`
+╭◪ *KERANG*
+╰───────────────────
+•├❍ *${prefix}apakah [optional]*
+•├❍ *${prefix}rate [optional]*
+•├❍ *${prefix}bisakah [optional]*
+•├❍ *${prefix}kapankah [optional]*
+•├❍ *${prefix}gantengcek*
+•├❍ *${prefix}toxic*
+•├❍ *${prefix}cantikcek*
+•├❍ *${prefix}persengay*
+ ╰───────────────────
+ ╰─────〘 Hazzelnut BOT 〙`
 }
 exports.kerangmenu = kerangmenu
