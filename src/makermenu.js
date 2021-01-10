19b32cea079ab081ca4a437d43d3bd1e21875465
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:44:54 +0700

    Update makermenu.js

--- src/makermenu.js
+++ src/makermenu.js
@@ -1,30 +1,30 @@
 const makermenu = (prefix) => { 
 	return `
-╔══✪〘 MAKER 〙✪══
-║
-╠➥ *${prefix}tahta* [iki]
-╠➥ *${prefix}pronlogo* [text|text]
-╠➥ *${prefix}snow* [text|text]
-╠➥ *${prefix}marvelogo* [text|text]
-╠➥ *${prefix}text3d* [text]
-╠➥ *${prefix}ninjalogo* [text|text]
-╠➥ *${prefix}wolflogo* [text|text]
-╠➥ *${prefix}lionlogo* [text|text]
-╠➥ *${prefix}textscreen* [text
-╠➥ *${prefix}rtext* [text]
-╠➥ *${prefix}thunder* [text]
-╠➥ *${prefix}stiltext* [text|text]
-╠➥ *${prefix}party* [text]
-╠➥ *${prefix}galaxtext* [text]
-╠➥ *${prefix}lovemake* [text]
-╠➥ *${prefix}walpaperhd* [text]
-╠➥ *${prefix}watercolor* [text]
-╠➥ *${prefix}quotemaker* [tx|tx|random]
-╠➥ *${prefix}water* [text]
-╠➥ *${prefix}epep* [text]
-╠➥ *${prefix}glitch* [text]
-╠➥ *${prefix}jokerlogo [teks]*
-║
-╚═〘 Hazzelnut BOT 〙`
+╭◪ *MAKER*
+╰───────────────────
+•├❍ *${prefix}tahta* [iki]
+•├❍ *${prefix}pronlogo* [text|text]
+•├❍ *${prefix}snow* [text|text]
+•├❍ *${prefix}marvelogo* [text|text]
+•├❍ *${prefix}text3d* [text]
+•├❍ *${prefix}ninjalogo* [text|text]
+•├❍ *${prefix}wolflogo* [text|text]
+•├❍ *${prefix}lionlogo* [text|text]
+•├❍ *${prefix}textscreen* [text
+•├❍ *${prefix}rtext* [text]
+•├❍ *${prefix}thunder* [text]
+•├❍ *${prefix}stiltext* [text|text]
+•├❍ *${prefix}party* [text]
+•├❍ *${prefix}galaxtext* [text]
+•├❍ *${prefix}lovemake* [text]
+•├❍ *${prefix}walpaperhd* [text]
+•├❍ *${prefix}watercolor* [text]
+•├❍ *${prefix}quotemaker* [tx|tx|random]
+•├❍ *${prefix}water* [text]
+•├❍ *${prefix}epep* [text]
+•├❍ *${prefix}glitch* [text]
+•├❍ *${prefix}jokerlogo [teks]*
+ ╰───────────────────
+ ╰─────〘 Hazzelnut BOT 〙`
 }
 exports.makermenu = makermenu
