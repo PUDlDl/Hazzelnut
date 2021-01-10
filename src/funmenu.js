8b4283541aa87b5cd6bce0081200f253cf13ef78
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:10:54 +0700

    Update funmenu.js

--- src/funmenu.js
+++ src/funmenu.js
@@ -1,21 +1,21 @@
 const funmenu = (prefix) => { 
 	return `
-╔══✪〘 FUN 〙✪══
-║
-╠➥ *${prefix}tebakgambar*
-╠➥ *${prefix}caklontong*
-╠➥ *${prefix}family100*
-╠➥ *${prefix}game*
-╠➥ *${prefix}truth*
-╠➥ *${prefix}dare*
-╠➥ *${prefix}quotes*
-╠➥ *${prefix}hilih*
-╠➥ *${prefix}alay* [text]
-╠➥ *${prefix}simi* [text]
-╠➥ *${prefix}bucin*
-╠➥ *${prefix}gtts* [text]
-╠➥ *${prefix}tts*
-║
-╚═〘 Hazzelnut BOT 〙`
+╭◪ *FUN*
+╰───────────────────
+•├❍ *${prefix}tebakgambar*
+•├❍ *${prefix}caklontong*
+•├❍ *${prefix}family100*
+•├❍ *${prefix}game*
+•├❍ *${prefix}truth*
+•├❍ *${prefix}dare*
+•├❍ *${prefix}quotes*
+•├❍ *${prefix}hilih*
+•├❍ *${prefix}alay* [text]
+•├❍ *${prefix}simi* [text]
+•├❍ *${prefix}bucin*
+•├❍ *${prefix}gtts* [text]
+•├❍ *${prefix}tts*
+ ╰───────────────────
+ ╰─────〘 Hazzelnut BOT 〙`
 }
 exports.funmenu = funmenu
