5bc94e1d4619364907e09dfaeaa8f37e948a7f3f
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:48:09 +0700

    Update othermenu.js

--- src/othermenu.js
+++ src/othermenu.js
@@ -1,44 +1,43 @@
 const othermenu = (prefix) => { 
 	return `
-╔══✪〘 OTHER 〙✪══
-║
-╠➥ *${prefix}sticker*
-╠➥ *${prefix}stickergif*
-╠➥ *${prefix}ttp [teks]*
-╠➥ *${prefix}toimg [replay sticker]*
-╠➥ *${prefix}neko*
-╠➥ *${prefix}pokemon*
-╠➥ *${prefix}inu*
-╠➥ *${prefix}infoGempa*
-╠➥ *${prefix}quotes*
-╠➥ *${prefix}dadu*
-╠➥ *${prefix}quotes*
-╠➥ *${prefix}thunder [teks]*
-╠➥ *${prefix}nulis [teks]*
-╠➥ *${prefix}ocr [gambar]*
-╠➥ *${prefix}meme*
-╠➥ *${prefix}memindo*
-╠➥ *${prefix}testime*
-╠➥ *${prefix}ttp [teks]*
-╠➥ *${prefix}hobby*
-╠➥ *${prefix}beritahoax*
-╠➥ *${prefix}watak*
-╠➥ *${prefix}jsholat [daerah]*
-╠➥ *${prefix}report*
-╠➥ *${prefix}hilih [teks]*
-╠➥ *${prefix}cekjodoh* [nama]
-╠➥ *${prefix}artinama* [rizky]
-╠➥ *${prefix}listsurah*
-╠➥ *${prefix}zodiak [zodiak kamu]*
-╠➥ *${prefix}listzodiak*
-║
-╠═══✪〘 ANIMALS 〙✪══
-║
-╠➥ *${prefix}unta*
-╠➥ *${prefix}elang*
-╠➥ *${prefix}anjing*
-╠➥ *${prefix}randomcat*
-║
-╚═〘 Hazzelnut BOT 〙`
+╭◪ *OTHER*
+╰───────────────────
+•├❍ *${prefix}sticker*
+•├❍ *${prefix}stickergif*
+•├❍ *${prefix}ttp [teks]*
+•├❍ *${prefix}toimg [replay sticker]*
+•├❍ *${prefix}neko*
+•├❍ *${prefix}pokemon*
+•├❍ *${prefix}inu*
+•├❍ *${prefix}infoGempa*
+•├❍ *${prefix}quotes*
+•├❍ *${prefix}dadu*
+•├❍ *${prefix}quotes*
+•├❍ *${prefix}thunder [teks]*
+•├❍ *${prefix}nulis [teks]*
+•├❍ *${prefix}ocr [gambar]*
+•├❍ *${prefix}meme*
+•├❍ *${prefix}memindo*
+•├❍ *${prefix}testime*
+•├❍ *${prefix}ttp [teks]*
+•├❍ *${prefix}hobby*
+•├❍ *${prefix}beritahoax*
+•├❍ *${prefix}watak*
+•├❍ *${prefix}jsholat [daerah]*
+•├❍ *${prefix}report*
+•├❍ *${prefix}hilih [teks]*
+•├❍ *${prefix}artinama* [hazzelnut]
+•├❍ *${prefix}listsurah*
+•├❍ *${prefix}zodiak [zodiak kamu]*
+•├❍ *${prefix}listzodiak*
+ ╰───────────────────
+
+╭◪ *ANIMALS*
+╰───────────────────
+•├❍ *${prefix}unta*
+•├❍ *${prefix}elang*
+•├❍ *${prefix}anjing*
+•├❍ *${prefix}randomcat*
+ ╰─〘 Hazzelnut BOT 〙`
 }
 exports.othermenu = othermenu
