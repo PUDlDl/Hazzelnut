86697a91d8b6d8dabf09fd719f649334330bafd5
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:46:08 +0700

    Update mediamenu.js

--- src/mediamenu.js
+++ src/mediamenu.js
@@ -1,42 +1,42 @@
 const mediamenu = (prefix) => { 
 	return `
 
-╔══✪〘 MEDIA 〙✪══
-║
-╠➥ *${prefix}yt* [link]
-╠➥ *${prefix}tiktok* [link]
-╠➥ *${prefix}ytsearch* [yt search]
-╠➥ *${prefix}lirik* [judul lagu]
-╠➥ *${prefix}chord* [judul lagu]
-╠➥ *${prefix}igstalk* [Rizky]
-╠➥ *${prefix}wikien* [love]
-╠➥ *${prefix}tiktokstalk* [username]
-╠➥ *${prefix}url2img* [link]
-╠➥ *${prefix}fototiktok* [username]
-╠➥ *${prefix}map* [kota]
-╠➥ *${prefix}kbbi* [kamus]
-╠➥ *${prefix}brainly* [tau sendiri kan]
-╠➥ *${prefix}infoghitub* 
-╠➥ *${prefix}infocuaca* [kota]
-╠➥ *${prefix}infogempa*
-╠➥ *${prefix}artinama [nama]*
-╠➥ *${prefix}covid [negara]*
-╠➥ *${prefix}nulis [teks]*
-╠➥ *${prefix}sandwriting [teks]*
-╠➥ *${prefix}quotemaker [|teks|author|theme]*
-╠➥ *${prefix}resepmasakan [optional]*
-╠➥ *${prefix}tts [kode bhs] [teks]*
-╠➥ *${prefix}igstalk [@username]*
-╠➥ *${prefix}tiktokstalk [@username]*
-╠➥ *${prefix}wiki [query]*
-╠➥ *${prefix}qrcode [optional]*
-╠➥ *${prefix}map [optional]*
-╠➥ *${prefix}textmaker [teks1|teks2]*
-╠➥ *${prefix}ssweb [linkWeb]*
-╠➥ *${prefix}shorturl [linkWeb]*
-╠➥ *${prefix}animesaran*
-╠➥ *${prefix}animesaran2*
-║
-╚═〘 Hazzelnut BOT 〙`
+╭◪ *MEDIA*
+╰───────────────────
+•├❍ *${prefix}yt* [link]
+•├❍ *${prefix}tiktok* [link]
+•├❍ *${prefix}ytsearch* [yt search]
+•├❍ *${prefix}lirik* [judul lagu]
+•├❍ *${prefix}chord* [judul lagu]
+•├❍ *${prefix}igstalk* [Rizky]
+•├❍ *${prefix}wikien* [love]
+•├❍ *${prefix}tiktokstalk* [username]
+•├❍ *${prefix}url2img* [link]
+•├❍ *${prefix}fototiktok* [username]
+•├❍ *${prefix}map* [kota]
+•├❍ *${prefix}kbbi* [kamus]
+•├❍ *${prefix}brainly* [tau sendiri kan]
+•├❍ *${prefix}infoghitub* 
+•├❍ *${prefix}infocuaca* [kota]
+•├❍ *${prefix}infogempa*
+•├❍ *${prefix}artinama [nama]*
+•├❍ *${prefix}covid [negara]*
+•├❍ *${prefix}nulis [teks]*
+•├❍ *${prefix}sandwriting [teks]*
+•├❍ *${prefix}quotemaker [|teks|author|theme]*
+•├❍ *${prefix}resepmasakan [optional]*
+•├❍ *${prefix}tts [kode bhs] [teks]*
+•├❍ *${prefix}igstalk [@username]*
+•├❍ *${prefix}tiktokstalk [@username]*
+•├❍ *${prefix}wiki [query]*
+•├❍ *${prefix}qrcode [optional]*
+•├❍ *${prefix}map [optional]*
+•├❍ *${prefix}textmaker [teks1|teks2]*
+•├❍ *${prefix}ssweb [linkWeb]*
+•├❍ *${prefix}shorturl [linkWeb]*
+•├❍ *${prefix}animesaran*
+•├❍ *${prefix}animesaran2*
+ ╰───────────────────
+ ╰─────〘 Hazzelnut BOT 〙`
 }
 exports.mediamenu = mediamenu
