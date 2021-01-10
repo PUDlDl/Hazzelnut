b13f3d510c56b6bf4bcf97894b642dd63ce05884
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:08:33 +0700

    Update daftarvip.js

--- src/daftarvip.js
+++ src/daftarvip.js
@@ -1,17 +1,18 @@
 const daftarvip = (prefix) => { 
 	return `
 	
-*HARGA DAFTAR VIP :*
--Rp. ?K > Akses Fitur ViP
--Rp. ??K > Fitur VIP + Masukin Bot KeGrup Kalian!
-
-*JIKA INGIN DAFTAR VIP :*
-
-*Chat Owner BOT :*
-_wa.me/6287714745440 atau ketik *${prefix}owner*
-
-*NOTE*
-
+ ╭◪ *HARGA DAFTAR VIP :*
+ ╰───────────────────
+•├❍-Rp. ?K > Akses Fitur ViP
+•├❍-Rp. ??K > Fitur VIP + Masukin Bot KeGrup Kalian!
+ ╰──────────────────────────────────────────
+ ╭◪ *JIKA INGIN DAFTAR VIP :*
+ ╰───────────────────
+•├❍ *Chat Owner BOT :*
+•├❍ wa.me/6287714745440 atau ketik *${prefix}owner*
+ ╰─────────────────────────────────────────
+ ╭◪ *NOTE*
+ ╰───────────────────
 Please wait for the manuscript in the manufacturing process `
 }
 exports.daftarvip = daftarvip
