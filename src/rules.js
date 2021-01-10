b11bc7712d134be90477b7f08fff3c56868031a9
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 09:01:23 +0700

    Update rules.js

--- src/rules.js
+++ src/rules.js
@@ -1,25 +1,47 @@
 const rules = (prefix) => { 
 	return `
-*RULES BAGI PENGGUNA BOT*
-    
+╭◪ *RULES BAGI PENGGUNA BOT*
+╰───────────────────   
 ├─❏ Tolong Gunakan Delay Jangan Spam Saat Menggunakan Bot, Mentang Mentang Gratis Diborong semua.
 ├─❏ Call/VC Bot Auto Block.
 ├─❏ Jangan Call/VC Bot Kalau Tidak aktif.
 ├─❏ Bot tidak aktif 24 jam, jadi tergantung kalau ownernya lagi ada waktu botnya juga on.
+╰───────────────────
 
-*Konsekuensi Bila Melanggar Rules*
-Bot Akan Memblokir Kamu Atau Keluar Dari Grup Yang Kamu Kelola.
+╭◪ *Konsekuensi Bila Melanggar Rules*
+├❍ Bot Akan Memblokir Kamu Atau Keluar Dari Grup Yang Kamu Kelola.
+╰───────────────────
 
-Rules ini untuk kenyamanan semua yang memakai
+╭◪ Rules ini untuk kenyamanan semua yang memakai
 bot ini 
-1. Jangan spam bot. 
-Sanksi: *WARN/SOFT BLOCK*
 
-2. Jangan telepon bot.
-Sanksi: *SOFT BLOCK*
+╭◪ 1. Jangan spam bot. 
+| Sanksi: *WARN/SOFT BLOCK*
+╰───────────────────
+╭◪ 2. Jangan telepon bot.
+| Sanksi: *SOFT BLOCK*
+╰───────────────────
+╭◪ 3. Jangan mengeksploitasi bot.
+| Sanksi: *PERMANENT BLOCK*
+╰───────────────────
 
-3. Jangan mengeksploitasi bot.
-Sanksi: *PERMANENT BLOCK*
+ ❍
+く|)へ <~~me
+  〉
+━━━━━┓ ＼＼
+┓┓┓┓┓┃ \
+┓┓┓┓┓┃  ヽ○ノ
+┓┓┓┓┓┃   /
+┓┓┓┓┓┃ ノ)  <-- you
+┓┓┓┓┓┃
+┓┓┓┓┓┃
+┓┓┓┓┓┃
+┓┓┓┓┓┃
+┓┓┓┓┓┃
+┓┓┓┓┓┃
+┓┓┓┓┓┃
+┓┓┓┓┓┃ 
+❏❏❏❏❏
 
 Jika sudah dipahami rules-nya, silakan ketik *${prefix}menu* untuk memulai!`
 }
