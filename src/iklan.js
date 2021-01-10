16ab71f74304e68b1249a94611c1cb340ee6419e
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 08:19:05 +0700

    Update iklan.js

--- src/iklan.js
+++ src/iklan.js
@@ -1,29 +1,34 @@
 const iklan = () => { 
 	return `           
-╔══✪〘 IKLAN 〙✪══
-║
-╠═══════════════════════════
-╠➥ *DAFTAR SEWA BOT :*
-╠➥ *SEWA : 10K/GRUP (BULAN)*
-╠➥ *SEWA : 30K (1 TAHUN)*
-╠➥ *PEMBAYARAN BISA MELALUI :*
-╠➥ *GOPAY, DANA, PULSA, SAWERIA*
-╠═══════════════════════════
-╠➥ *KEUNTUNGAN SEWA BOT :*
-╠➥ *1. BISA MENJADI ADMIN Hazzelnut*
-╠➥ *2. BISA MENDAPATKAN COMMAND ADMIN*
-╠➥ *KEUNTUNGAN BUAT BOT :*
-╠➥ *1. BISA MENJADI OWNER BOT SENDIRI*
-╠➥ *2. BISA MENGGANTI NAMA BOT SENDIRI*
-╠➥ *3. BISA MEMBAWA BOT KE GROUP*
-╠➥ *4. BISA MENGGUNAKAN COMMAND OWNER*
-╠➥ *5. BISA MENYEWAKAN BOT KEMBALI*
-╠═══════════════════════════
-╠➥ *JIKA MINAT IKLAN DIATAS*
-╠➥ *HARAP HUBUNGI NOMOR DIBAWAH :*
-╠➥ *wa.me/6287714745440*
-║
-╚═〘 Hazzelnut BOT 〙
+╭◪ *IKLAN*
+╰───────────────────────────
+•├❍ *DAFTAR SEWA BOT :*
+•├❍ *SEWA : ??K/GRUP (BULAN)*
+•├❍ *SEWA : ??K (1 TAHUN)*
+•├❍ *PEMBAYARAN BISA MELALUI :*
+•├❍ *GOPAY, DANA, PULSA, SAWERIA*
+ ╰──────────────────────────
+
+╭◪ *KEUNTUNGAN SEWA BOT :*
+╰───────────────────────────
+•├❍ *1. BISA MENJADI ADMIN Hazzelnut*
+•├❍ *2. BISA MENDAPATKAN COMMAND ADMIN*
+ ╰───────────────────────────
+
+╭◪ *KEUNTUNGAN BUAT BOT :*
+╰────────────────────────────
+•├❍ *1. BISA MENJADI OWNER BOT SENDIRI*
+•├❍ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+•├❍ *3. BISA MEMBAWA BOT KE GROUP*
+•├❍ *4. BISA MENGGUNAKAN COMMAND OWNER*
+•├❍ *5. BISA MENYEWAKAN BOT KEMBALI*
+ ╰───────────────────────────
+
+╭◪ *JIKA MINAT IKLAN DIATAS*
+╰────────────────────────────
+•├❍ *HARAP HUBUNGI NOMOR DIBAWAH :*
+•├❍ *wa.me/6287714745440*
+ ╰──────────────────〘 Hazzelnut BOT 〙
 `
 }
 exports.iklan = iklan
