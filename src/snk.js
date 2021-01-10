276b92114746e3791077f383b06a159ea0617104
PUDIDI ae lah <76520864+PUDlDl@users.noreply.github.com>
2021-01-10 21:59:40 +0700

    Update snk.js

--- src/snk.js
+++ src/snk.js
@@ -1,5 +1,5 @@
 const snk = () => { 
-	return `Syarat dan Ketentuan Bot *Rizky* 
+	return `Syarat dan Ketentuan Bot *Hazzelnut* 
 1. Kami tidak menyimpan gambar, video, file, audio, dan dokumen yang anda kirim
 2. Kami tidak akan pernah meminta anda untuk memberikan informasi pribadi
 3. Jika menemukan Bug/Error silahkan langsung lapor ke Owner bot
